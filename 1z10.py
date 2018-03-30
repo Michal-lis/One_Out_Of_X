6f618a4624070287fb1d83665fe39945dd12d025
michal <michallis95@vp.pl>
2018-03-30 17:17:11 +0200

    images and page added

--- 1z10.py
+++ 1z10.py
@@ -1,45 +1,44 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from pages import StartPage
 
-# created by mlis 30.01.2018
-# modules used pillow,tkinter
-
-LARGE_FONT = ("Verdana", 12)
+"""
+created by mlis 30.01.2018
+pypi modules used: pillow,tkinter
+"""
 
 
 class TopLevelWidget(Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
+        self.title("1 z 10 - The Game")
+        self.iconbitmap(self, default="images/logo.ico")
 
+        self.init_global_container()
+
+        page_list = [StartPage]
+        self.init_pages(page_list)
+
+        self.show_frame(StartPage)
+
+    def init_pages(self, pages):
+        for F in pages:
+            frame = F(self, self.container)
+            frame.pack()
+            self.frames[F] = frame
+
+    def init_global_container(self):
         # container is a frame containting all the other fremes
         container = Frame(self)
         container.pack(side="top", fill="both", expand=True)
         # minimum size, priority
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
+        self.container = container
         self.frames = {}
-
-        frame = StartPage(container, self)
-        frame.pack()
-
-        self.frames[StartPage] = frame
-        self.show_frame(StartPage)
 
     def show_frame(self, frame_key):
         frame = self.frames[frame_key]
         frame.tkraise()
-
-
-class StartPage(Frame):
-    def __init__(self, parent, controller):
-        Frame.__init__(self, parent)
-
-        label = Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1 = Button(self, text="Visit Page 1", command=lambda: exit())
-        button1.pack()
 
 
 root = TopLevelWidget()
