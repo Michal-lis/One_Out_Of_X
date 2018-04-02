8f534624daecacf087855faf73d14744032a8c1f
michal <michallis95@vp.pl>
2018-04-02 14:41:00 +0200

    Final Stage

---
+++ question_parser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import zipfile
+
+myfile = zipfile.ZipFile('s79 e01.odt')
+listoffiles = myfile.infolist()
+for s in listoffiles:
+    if s.orig_filename == 'content.xml':
+        fd = open('example.xml', 'w')
+        bh = myfile.read(s.orig_filename)
+        fd.write(bh)
+        fd.close()
