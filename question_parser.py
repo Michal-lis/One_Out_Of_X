a7b3fac14271b70bce8ec942abcb3cf0787f4631
michal <michallis95@vp.pl>
2018-04-02 15:17:49 +0200

    parsed question

--- question_parser.py
+++ question_parser.py
@@ -6,7 +6,7 @@ myfile = zipfile.ZipFile('s79 e01.odt')
 listoffiles = myfile.infolist()
 for s in listoffiles:
     if s.orig_filename == 'content.xml':
-        fd = open('example.xml', 'w')
+        fd = open('example.xml', 'wb')
         bh = myfile.read(s.orig_filename)
         fd.write(bh)
         fd.close()
