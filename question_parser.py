36ccd06eb91141a6bb1679f8a2e322a1cc5d36c9
michal <michallis95@vp.pl>
2018-04-02 15:31:49 +0200

    parser v1

--- question_parser.py
+++ question_parser.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import zipfile
+import xml.etree.ElementTree as et
+from pprint import pprint
 
-myfile = zipfile.ZipFile('s79 e01.odt')
-listoffiles = myfile.infolist()
-for s in listoffiles:
-    if s.orig_filename == 'content.xml':
-        fd = open('example.xml', 'wb')
-        bh = myfile.read(s.orig_filename)
-        fd.write(bh)
-        fd.close()
+odt_name = 's79 e01.odt'
+
+
+def save_odt_to_xml(filename):
+    myfile = zipfile.ZipFile(filename)
+    listoffiles = myfile.infolist()
+    for s in listoffiles:
+        if s.orig_filename == 'content.xml':
+            fd = open('example.xml', 'wb')
+            bh = myfile.read(s.orig_filename)
+            pprint(bh)
+            fd.write(bh)
+            fd.close()
+
+
+def save_odt_to_database(filename):
+    myfile = zipfile.ZipFile(filename)
+    listoffiles = myfile.infolist()
+    for s in listoffiles:
+        if s.orig_filename == 'content.xml':
+            bh = myfile.read(s.orig_filename)
+            tree = et.parse(bh)
+            root = tree.getroot()
+            el = root.find('office')
+            pprint(el)
+
+
+if __name__ == '__main__':
+    save_odt_to_database(odt_name)
